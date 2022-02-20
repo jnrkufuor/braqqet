eebe72c9a5cf74f5bc26ae749a8276ef16eb0f19
Ernest Kufuor <jaycuffs@gmail.com>
2022-02-20 19:51:46 +0000

    Accrodions designed

--- static/js/script.js
+++ static/js/script.js
@@ -1,17 +1,37 @@
-$(document).ready(function(){   
-    
-    
-    var scroll_pos = 0;
-    $(document).scroll(function() { 
-        scroll_pos = $(this).scrollTop();
-        if(scroll_pos > 150) {
-            $(".header").css('background-color', 'rgba(255,255,255,0.85)');
-        } else {
-            $(".header").css('background-color', 'rgba(255,255,255,0)');
-        }
+$(document).ready(function () {
+  var scroll_pos = 0;
+  $(document).scroll(function () {
+    scroll_pos = $(this).scrollTop();
+    if (scroll_pos > 50) {
+      $(".header").css("background-color", "rgba(255,255,255,0.85)");
+    } else {
+      $(".header").css("background-color", "rgba(255,255,255,0)");
+    }
+  });
+
+  // Add minus icon for collapse element which
+  // is open by default
+  $(".collapse.show").each(function () {
+    $(this).prev(".card-header").find("i").attr("data-feather", "plus");
+    console.log($(this).prev(".card-header").find("i"));
+  });
+
+  // Toggle plus minus icon on show hide
+  // of collapse element
+  $(".collapse")
+    .on("show.bs.collapse", function () {
+      $(this).prev(".card-header").find("#plus_icon").attr("data-feather", "plus");;
+      console.log($(this).prev(".card-header").find("#plus_icon"));
+    })
+    .on("hide.bs.collapse", function () {
+      $(this).prev(".card-header").find("#plus_icon").attr("data-feather", "minus");
     });
 
-    $(".logo").click(function() {
-        // window.location.href=window.location.hostname;
-    });
+    $(".join-community").click(function () {
+        window.location.href="https://t.me/+R4xPtKA62lIyNTRk";
+    })
+
+  $(".logo").click(function () {
+    // window.location.href=window.location.hostname;
+  });
 });
